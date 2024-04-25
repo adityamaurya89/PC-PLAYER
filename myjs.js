4e220f33f9bb7376775fa81a1982b72ebb98dc67
anurodhmaurya89 <adityamauryaam861@gmail.com>
2024-04-25 17:13:19 +0530

    first commit

---
+++ myjs.js
@@ -0,0 +1,11 @@
+document.write(" this is my first javascript code")
+
+ var x = 50;
+ console.log(x);
+ console.log([1,2,3]);
+ console.table([1, 2, 3]);
+ console.error("Something went wrong.");
+ console.time("Test");
+ console.warn("This is just warning");
+ console.timeEnd("Test");
+ //console.clear();
